1d1557375a9a2a9b2aabd44b0a0e8c9972a233fd
Thishon <127035249+GTLdd@users.noreply.github.com>
2024-03-31 15:42:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+
+function hideContent(id, btnId){
+    var contentDiv = document.getElementById(id);
+    var btn = document.getElementById(btnId);
+    
+
+    if(contentDiv.style.display != 'block'){
+        contentDiv.style.display = 'block';
+        contentDiv.style.transition = '2s';
+        btn.style.marginTop = '-10px'
+    }else{
+        contentDiv.style.display = 'none';
+        btn.style.marginTop = '25px'
+
+    }
+}
+
+
+/*function hideContent(id, gridId, outerGridId){
+    var contentDiv = document.getElementById(id);
+    var grid = document.getElementById(gridId)
+    var outerGrid = document.getElementById(outerGridId)
+    
+
+    if(contentDiv.style.display != 'flex'){
+        contentDiv.style.display = 'flex';
+        grid.style.marginTop = '100px';
+        outerGrid.style.marginTop = '700px'
+
+    }else{
+        contentDiv.style.display = 'none';
+        grid.style.marginTop = '0px';
+        outerGrid.style.marginTop = '580px'
+    }
+}*/
